b783f244114da94d8454d0886604693230838e12
Gregory Daynes <gregdaynes@gmail.com>
2025-04-24 23:31:36 -0700

    chore: vendor current modules

---
+++ vendor/github.com/danielgtaylor/huma/v2/adapters/humaflow/humaflow.go
@@ -0,0 +1,185 @@
+package humaflow
+
+import (
+	"context"
+	"crypto/tls"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humaflow/flow"
+	"github.com/danielgtaylor/huma/v2/queryparam"
+)
+
+// MultipartMaxMemory is the maximum memory to use when parsing multipart
+// form data.
+var MultipartMaxMemory int64 = 8 * 1024
+
+// Unwrap extracts the underlying HTTP request and response writer from a Huma
+// context. If passed a context from a different adapter it will panic.
+func Unwrap(ctx huma.Context) (*http.Request, http.ResponseWriter) {
+	if c, ok := ctx.(*goContext); ok {
+		return c.Unwrap()
+	}
+	panic("not a humaflow context")
+}
+
+type goContext struct {
+	op     *huma.Operation
+	r      *http.Request
+	w      http.ResponseWriter
+	status int
+}
+
+// check that goContext implements huma.Context
+var _ huma.Context = &goContext{}
+
+func (c *goContext) Unwrap() (*http.Request, http.ResponseWriter) {
+	return c.r, c.w
+}
+
+func (c *goContext) Operation() *huma.Operation {
+	return c.op
+}
+
+func (c *goContext) Context() context.Context {
+	return c.r.Context()
+}
+
+func (c *goContext) Method() string {
+	return c.r.Method
+}
+
+func (c *goContext) Host() string {
+	return c.r.Host
+}
+
+func (c *goContext) RemoteAddr() string {
+	return c.r.RemoteAddr
+}
+
+func (c *goContext) URL() url.URL {
+	return *c.r.URL
+}
+
+func (c *goContext) Param(name string) string {
+	return flow.Param(c.r.Context(), name)
+}
+
+func (c *goContext) Query(name string) string {
+	return queryparam.Get(c.r.URL.RawQuery, name)
+}
+
+func (c *goContext) Header(name string) string {
+	return c.r.Header.Get(name)
+}
+
+func (c *goContext) EachHeader(cb func(name, value string)) {
+	for name, values := range c.r.Header {
+		for _, value := range values {
+			cb(name, value)
+		}
+	}
+}
+
+func (c *goContext) BodyReader() io.Reader {
+	return c.r.Body
+}
+
+func (c *goContext) GetMultipartForm() (*multipart.Form, error) {
+	err := c.r.ParseMultipartForm(MultipartMaxMemory)
+	return c.r.MultipartForm, err
+}
+
+func (c *goContext) SetReadDeadline(deadline time.Time) error {
+	return huma.SetReadDeadline(c.w, deadline)
+}
+
+func (c *goContext) SetStatus(code int) {
+	c.status = code
+	c.w.WriteHeader(code)
+}
+
+func (c *goContext) Status() int {
+	return c.status
+}
+
+func (c *goContext) AppendHeader(name string, value string) {
+	c.w.Header().Add(name, value)
+}
+
+func (c *goContext) SetHeader(name string, value string) {
+	c.w.Header().Set(name, value)
+}
+
+func (c *goContext) BodyWriter() io.Writer {
+	return c.w
+}
+
+func (c *goContext) TLS() *tls.ConnectionState {
+	return c.r.TLS
+}
+
+func (c *goContext) Version() huma.ProtoVersion {
+	return huma.ProtoVersion{
+		Proto:      c.r.Proto,
+		ProtoMajor: c.r.ProtoMajor,
+		ProtoMinor: c.r.ProtoMinor,
+	}
+}
+
+// NewContext creates a new Huma context from an HTTP request and response.
+func NewContext(op *huma.Operation, r *http.Request, w http.ResponseWriter) huma.Context {
+	return &goContext{op: op, r: r, w: w}
+}
+
+type Mux interface {
+	HandleFunc(pattern string, handler http.HandlerFunc, methods ...string)
+	ServeHTTP(http.ResponseWriter, *http.Request)
+}
+
+type goAdapter struct {
+	Mux
+	prefix string
+}
+
+func (a *goAdapter) Handle(op *huma.Operation, handler func(huma.Context)) {
+	// Convert {param} to :param
+	path := op.Path
+	path = strings.ReplaceAll(path, "{", ":")
+	path = strings.ReplaceAll(path, "}", "")
+	a.HandleFunc(a.prefix+path, func(w http.ResponseWriter, r *http.Request) {
+		handler(&goContext{op: op, r: r, w: w})
+	}, op.Method)
+}
+
+// NewAdapter creates a new adapter for the given chi router.
+func NewAdapter(m Mux) huma.Adapter {
+	return &goAdapter{Mux: m}
+}
+
+// New creates a new Huma API using an HTTP mux.
+//
+//	mux := http.NewServeMux()
+//	api := humago.New(mux, huma.DefaultConfig("My API", "1.0.0"))
+func New(m Mux, config huma.Config) huma.API {
+	return huma.NewAPI(config, &goAdapter{m, ""})
+}
+
+// NewWithPrefix creates a new Huma API using an HTTP mux with a URL prefix.
+// This behaves similar to other router's group functionality, adding the prefix
+// before each route path (but not in the OpenAPI). The prefix should be used in
+// combination with the `OpenAPI().Servers` base path to ensure the correct URLs
+// are generated in the OpenAPI spec.
+//
+//	mux := flow.New()
+//	config := huma.DefaultConfig("My API", "1.0.0")
+//	config.Servers = []*huma.Server{{URL: "http://example.com/api"}}
+//	api := humago.NewWithPrefix(mux, "/api", config)
+func NewWithPrefix(m Mux, prefix string, config huma.Config) huma.API {
+	return huma.NewAPI(config, &goAdapter{m, prefix})
+}
---
+++ vendor/github.com/danielgtaylor/huma/v2/humatest/humatest.go
@@ -0,0 +1,404 @@
+// Package humatest provides testing utilities for Huma services. It is based on
+// the standard library `http.Request` & `http.ResponseWriter` types.
+package humatest
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"reflect"
+	"strings"
+	"testing/iotest"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humaflow"
+	"github.com/danielgtaylor/huma/v2/adapters/humaflow/flow"
+)
+
+// TB is a subset of the `testing.TB` interface used by the test API and
+// implemented by the `*testing.T` and `*testing.B` structs.
+type TB interface {
+	Helper()
+	Log(args ...any)
+	Logf(format string, args ...any)
+}
+
+// NewContext creates a new test context from an HTTP request and response.
+func NewContext(op *huma.Operation, r *http.Request, w http.ResponseWriter) huma.Context {
+	return humaflow.NewContext(op, r, w)
+}
+
+// NewAdapter creates a new test adapter from a router.
+func NewAdapter() huma.Adapter {
+	return humaflow.NewAdapter(flow.New())
+}
+
+// TestAPI is a `huma.API` with additional methods specifically for testing.
+type TestAPI interface {
+	huma.API
+
+	// DoCtx a request against the API with a custom [context.Context] in the
+	// [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	DoCtx(ctx context.Context, method, path string, args ...any) *httptest.ResponseRecorder
+
+	// Do a request against the API using [context.Background] in the [http.Request].
+	// Args, if provided, should be string headers like `Content-Type:
+	// application/json`, an `io.Reader` for the request body, or a slice/map/struct
+	// which will be serialized to JSON and sent as the request body. Anything else
+	// will panic.
+	Do(method, path string, args ...any) *httptest.ResponseRecorder
+
+	// GetCtx performs a GET request against the API with a custom [context.Context]
+	// in the [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a GET request
+	//	api.GetCtx(ctx, "/foo")
+	//
+	// 	// Make a GET request with a custom header.
+	// 	api.GetCtx(ctx, "/foo", "X-My-Header: my-value")
+	GetCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder
+
+	// Get performs a GET request against the API using [context.Background] in the
+	// [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a GET request
+	// 	api.Get("/foo")
+	//
+	// 	// Make a GET request with a custom header.
+	// 	api.Get("/foo", "X-My-Header: my-value")
+	Get(path string, args ...any) *httptest.ResponseRecorder
+
+	// PostCtx performs a POST request against the API with a custom
+	// [context.Context] in the [http.Request]. Args, if provided, should be string
+	// headers like `Content-Type: application/json`, an `io.Reader` for the request
+	// body, or a slice/map/struct which will be serialized to JSON and sent as the
+	// request body. Anything else will panic.
+	//
+	// 	// Make a POST request
+	// 	api.PostCtx(ctx, "/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a POST request with a custom header.
+	// 	api.PostCtx(ctx, "/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	PostCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder
+
+	// Post performs a POST request against the API using [context.Background] in the
+	// [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a POST request
+	//	api.Post("/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a POST request with a custom header.
+	// 	api.Post("/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	Post(path string, args ...any) *httptest.ResponseRecorder
+
+	// PutCtx performs a PUT request against the API with a custom [context.Context]
+	// in the [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a PUT request
+	// 	api.PutCtx(ctx, "/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a PUT request with a custom header.
+	// 	api.PutCtx(ctx, "/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	PutCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder
+
+	// Put performs a PUT request against the API using [context.Background] in the
+	// [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a PUT request
+	// 	api.Put("/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a PUT request with a custom header.
+	// 	api.Put("/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	Put(path string, args ...any) *httptest.ResponseRecorder
+
+	// PatchCtx performs a PATCH request against the API with a custom
+	// [context.Context] in the [http.Request]. Args, if provided, should be string
+	// headers like `Content-Type: application/json`, an `io.Reader` for the request
+	// body, or a slice/map/struct which will be serialized to JSON and sent as the
+	// request body. Anything else will panic.
+	//
+	// 	// Make a PATCH request
+	// 	api.PatchCtx(ctx, "/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a PATCH request with a custom header.
+	// 	api.PatchCtx(ctx, "/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	PatchCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder
+
+	// Patch performs a PATCH request against the API using [context.Background] in
+	// the [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a PATCH request
+	// 	api.Patch("/foo", strings.NewReader(`{"foo": "bar"}`))
+	//
+	// 	// Make a PATCH request with a custom header.
+	// 	api.Patch("/foo", "X-My-Header: my-value", MyBody{Foo: "bar"})
+	Patch(path string, args ...any) *httptest.ResponseRecorder
+
+	// DeleteCtx performs a DELETE request against the API with a custom
+	// [context.Context] in the [http.Request]. Args, if provided, should be string
+	// headers like `Content-Type: application/json`, an `io.Reader` for the request
+	// body, or a slice/map/struct which will be serialized to JSON and sent as the
+	// request body. Anything else will panic.
+	//
+	// 	// Make a DELETE request
+	// 	api.DeleteCtx(ctx, "/foo")
+	//
+	// 	// Make a DELETE request with a custom header.
+	// 	api.DeleteCtx(ctx, "/foo", "X-My-Header: my-value")
+	DeleteCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder
+
+	// Delete performs a DELETE request against the API using [context.Background] in
+	// the [http.Request]. Args, if provided, should be string headers like
+	// `Content-Type: application/json`, an `io.Reader` for the request body, or a
+	// slice/map/struct which will be serialized to JSON and sent as the request
+	// body. Anything else will panic.
+	//
+	// 	// Make a DELETE request
+	//	api.Delete("/foo")
+	//
+	// 	// Make a DELETE request with a custom header.
+	// 	api.Delete("/foo", "X-My-Header: my-value")
+	Delete(path string, args ...any) *httptest.ResponseRecorder
+}
+
+var _ TestAPI = &testAPI{}
+
+type testAPI struct {
+	huma.API
+	tb TB
+}
+
+func (a *testAPI) Do(method, path string, args ...any) *httptest.ResponseRecorder {
+	return a.DoCtx(context.Background(), method, path, args...)
+}
+
+func (a *testAPI) DoCtx(ctx context.Context, method, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	var b io.Reader
+	isJSON := false
+	for _, arg := range args {
+		kind := reflect.Indirect(reflect.ValueOf(arg)).Kind()
+		if reader, ok := arg.(io.Reader); ok {
+			b = reader
+			break
+		} else if _, ok := arg.(string); ok {
+			// do nothing
+		} else if kind == reflect.Struct || kind == reflect.Map || kind == reflect.Slice {
+			encoded, err := json.Marshal(arg)
+			if err != nil {
+				panic(err)
+			}
+			b = bytes.NewReader(encoded)
+			isJSON = true
+		} else {
+			panic("unsupported argument type, expected string header or io.Reader/slice/map/struct body")
+		}
+	}
+
+	req, _ := http.NewRequestWithContext(ctx, method, path, b)
+	req.RequestURI = path
+	req.RemoteAddr = "127.0.0.1:12345"
+	if isJSON {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	for _, arg := range args {
+		if s, ok := arg.(string); ok {
+			parts := strings.Split(s, ":")
+			req.Header.Set(parts[0], strings.TrimSpace(strings.Join(parts[1:], ":")))
+
+			if strings.ToLower(parts[0]) == "host" {
+				req.Host = strings.TrimSpace(parts[1])
+			}
+		}
+	}
+	resp := httptest.NewRecorder()
+
+	bytes, _ := DumpRequest(req)
+	a.tb.Log("Making request:\n" + strings.TrimSpace(string(bytes)))
+
+	a.Adapter().ServeHTTP(resp, req)
+
+	bytes, _ = DumpResponse(resp.Result())
+	a.tb.Log("Got response:\n" + strings.TrimSpace(string(bytes)))
+
+	return resp
+}
+
+func (a *testAPI) Get(path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.GetCtx(context.Background(), path, args...)
+}
+
+func (a *testAPI) GetCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DoCtx(ctx, http.MethodGet, path, args...)
+}
+
+func (a *testAPI) Post(path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.PostCtx(context.Background(), path, args...)
+}
+
+func (a *testAPI) PostCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DoCtx(ctx, http.MethodPost, path, args...)
+}
+
+func (a *testAPI) Put(path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.PutCtx(context.Background(), path, args...)
+}
+
+func (a *testAPI) PutCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DoCtx(ctx, http.MethodPut, path, args...)
+}
+
+func (a *testAPI) Patch(path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.PatchCtx(context.Background(), path, args...)
+}
+
+func (a *testAPI) PatchCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DoCtx(ctx, http.MethodPatch, path, args...)
+}
+
+func (a *testAPI) Delete(path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DeleteCtx(context.Background(), path, args...)
+}
+
+func (a *testAPI) DeleteCtx(ctx context.Context, path string, args ...any) *httptest.ResponseRecorder {
+	a.tb.Helper()
+	return a.DoCtx(ctx, http.MethodDelete, path, args...)
+}
+
+// Wrap returns a `TestAPI` wrapping the given API.
+func Wrap(tb TB, api huma.API) TestAPI {
+	return &testAPI{api, tb}
+}
+
+// New creates a new router and test API, making it easy to register operations
+// and perform requests against them. Optionally takes a configuration object
+// to customize how the API is created. If no configuration is provided then
+// a simple default configuration supporting `application/json` is used.
+func New(tb TB, configs ...huma.Config) (http.Handler, TestAPI) {
+	for _, config := range configs {
+		if config.OpenAPI == nil {
+			panic("custom huma.Config structs must specify a value for OpenAPI")
+		}
+	}
+	if len(configs) == 0 {
+		configs = append(configs, huma.Config{
+			OpenAPI: &huma.OpenAPI{
+				Info: &huma.Info{
+					Title:   "Test API",
+					Version: "1.0.0",
+				},
+			},
+			Formats: map[string]huma.Format{
+				"application/json": huma.DefaultJSONFormat,
+				"json":             huma.DefaultJSONFormat,
+			},
+			DefaultFormat: "application/json",
+		})
+	}
+	r := flow.New()
+	return r, Wrap(tb, humaflow.New(r, configs[0]))
+}
+
+func dumpBody(body io.ReadCloser, buf *bytes.Buffer) (io.ReadCloser, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	b, err := io.ReadAll(body)
+	if err != nil {
+		return io.NopCloser(iotest.ErrReader(err)), err
+	}
+	body.Close()
+	if strings.Contains(buf.String(), "json") {
+		if err := json.Indent(buf, b, "", "  "); err != nil {
+			// Indent failed, so just write the buffer.
+			buf.Write(b)
+		}
+	} else {
+		buf.Write(b)
+	}
+	return io.NopCloser(bytes.NewReader(b)), nil
+}
+
+// DumpRequest returns a string representation of an HTTP request, automatically
+// pretty printing JSON bodies for readability.
+func DumpRequest(req *http.Request) ([]byte, error) {
+	var buf bytes.Buffer
+	b, err := httputil.DumpRequest(req, false)
+
+	if err == nil {
+		buf.Write(b)
+		req.Body, err = dumpBody(req.Body, &buf)
+	}
+
+	return buf.Bytes(), err
+}
+
+// DumpResponse returns a string representation of an HTTP response,
+// automatically pretty printing JSON bodies for readability.
+func DumpResponse(resp *http.Response) ([]byte, error) {
+	var buf bytes.Buffer
+	b, err := httputil.DumpResponse(resp, false)
+
+	if err == nil {
+		buf.Write(b)
+		resp.Body, err = dumpBody(resp.Body, &buf)
+	}
+
+	return buf.Bytes(), err
+}
+
+// PrintRequest prints a string representation of an HTTP request to stdout,
+// automatically pretty printing JSON bodies for readability.
+func PrintRequest(req *http.Request) {
+	b, _ := DumpRequest(req)
+	// Turn `/r/n` into `/n` for more straightforward output that is also
+	// compatible with Go's testable examples.
+	b = bytes.ReplaceAll(b, []byte("\r"), []byte(""))
+	fmt.Println(string(b))
+}
+
+// PrintResponse prints a string representation of an HTTP response to stdout,
+// automatically pretty printing JSON bodies for readability.
+func PrintResponse(resp *http.Response) {
+	b, _ := DumpResponse(resp)
+	// Turn `/r/n` into `/n` for more straightforward output that is also
+	// compatible with Go's testable examples.
+	b = bytes.ReplaceAll(b, []byte("\r"), []byte(""))
+	fmt.Println(string(b))
+}
